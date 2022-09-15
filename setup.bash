a0880f1ad0a378053c66ddf7ef589210deb156c1
Andre Ferreira <delcferreira@gmail.com>
2022-09-14 22:33:26 -0300

    fix: add echo into the

--- setup.bash
+++ setup.bash
@@ -12,10 +12,10 @@ apt-get install curl -y
 # install vim
 apt-get install vim -y
 
-# setup zsh 
-apt-get install zsh -y
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-ehco "zsh" >> ~/.bash
-
 # setup and install nvm
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+source ~/.bashrc
+
+# setup zsh 
+apt-get install zsh -y
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh) && ehco "zsh" >> ~/.bash"
