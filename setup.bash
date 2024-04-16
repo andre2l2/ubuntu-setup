a1f770f054933a209ee8b7c3fb8eb3fcd7cac910
Andre Ferreira <delcferreira@gmail.com>
2024-04-15 21:39:18 -0300

    fix: add flatpack apps

--- setup.bash
+++ setup.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tools=(
+apt_apps=(
   curl 
   vim 
   git
@@ -12,16 +12,39 @@ web_tools=(
   https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh
 )
 
+flatpak_apps=(
+  "com.google.Chrome"
+  "com.spotify.Client"
+  "com.discordapp.Discord"
+  "org.videolan.VLC"
+  "com.obsproject.Studio"
+  "com.usebruno.Bruno"
+  "io.beekeeperstudio.Studio"
+  "com.mongodb.Compass"
+  "md.obsidian.Obsidian"
+  "com.github.tchx84.Flatseal"
+  "org.flameshot.Flameshot"
+  "com.uploadedlobster.peek"
+  "com.jgraph.drawio.desktop"
+)
+
 clear
 
 # update
 echo "[UPDATEING]"
-$(apt-get update)
+$(apt-get update && flatpak update)
 
-# install tools
-for tool in ${tools[@]};
+# Install flatpaks
+for tool in ${flatpak_apps[@]}
 do
-  clear
+  echo "[INSTALLING] $tool"
+  $(flatpak install flathub $tool)
+  echo "\n"
+done 
+
+# Install apt apps
+for tool in ${apt_apps[@]};
+do
   echo "[INSTALLING] $tool"
   $(apt-get install -y $tool)
 done
